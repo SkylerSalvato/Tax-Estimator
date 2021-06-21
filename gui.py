3180b7fc356f3801ac0e76810b7ee08eaef6e9c5
Skyler Salvato <skylersalvato@gmail.com>
2021-06-21 02:42:40 -0400

    Added comments and organized code, removed borders and margins, updated close button, fixed some styling issues.

--- gui.py
+++ gui.py
@@ -1,128 +1,94 @@
 import sys
 
 # Qt widgets
-"""from PyQt6.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget,
-    QPushButton, QToolTip, QMessageBox, QMainWindow, QSizePolicy)
-from PyQt6.QtGui import QFont, QIcon, QAction
-
-class Example(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUi()
-
-    def initUi(self):
-        exitAct = QAction(QIcon('exit24.png'), 'Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.triggered.connect(QApplication.instance().quit)
-        
-        spacer = QWidget(self)
-        print(spacer.expandingDirections())
-        #spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-        self.toolbar = self.addToolBar('Exit')
-        self.toolbar.setMovable(False)
-        self.toolbar.addWidget(spacer)
-        self.toolbar.addAction(exitAct)
-
-        self.resize(350, 250)
-        self.setWindowTitle('Testing Qt')
-        self.setStyleSheet("background-color: #373F47; color: white;")
-        self.show()
-
-    def closeEvent(self, event):
-
-        reply = QMessageBox.question(self, 'Message',
-                        "Are you sure to quit?", QMessageBox.StandardButton.Yes |
-                        QMessageBox.StandardButton.No, QMessageBox.StandardButton.No)
-
-        if reply == QMessageBox.StandardButton.Yes:
-
-            event.accept()
-        else:
-
-            event.ignore()
-
-
-if __name__ == '__main__':
-    # Instantiate Qt
-    app = QApplication(sys.argv)
-
-    # Create GUI
-    window = QWidget()
-    window.setWindowTitle('PyQt6 App')
-    window.setGeometry(100, 100, 280, 80)
-    window.move(60, 15)
-    helloMsg = QLabel('<h1>Hello world!</h1>', parent=window)
-    helloMsg.move(60, 15)
-
-    # Show GUI
-    window.show()
- 
-    ex = Example()
-
-    # Run event loop
-    sys.exit(app.exec())"""
 from PyQt6.QtWidgets import (QLabel, QMainWindow, QWidget, QPushButton,
     QHBoxLayout, QVBoxLayout, QApplication, QStackedWidget)
-from PyQt6.QtCore import QPropertyAnimation, QRect, Qt
+from PyQt6.QtCore import QPoint, QPropertyAnimation, QRect, Qt
 from PyQt6.QtGui import QCursor, QIcon
 
-class Example(QWidget):
+# Main Window
+class TaxGui(QWidget):
 
+    # Call Parent, remove frames, and create signals
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.stacked_widget.currentChanged.connect(self.set_button_state)
         self.next_button.clicked.connect(self.next_page)
         self.prev_button.clicked.connect(self.prev_page)
 
-
+    # Setup the application parts
     def initUI(self):
-
+        
+        # Create the widgets
         self.next_button = QPushButton('Next')
-        self.next_button.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.prev_button = QPushButton('Previous')
-        self.prev_button.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        close = QPushButton('')
+        self.stacked_widget = QStackedWidget()
+        
+        # Configure the widgets
+        self.next_button.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.next_button.setEnabled(False)
+        self.prev_button.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.prev_button.setEnabled(False)
+        #close.setIcon(QIcon('exit24.png'))
+        close.clicked.connect(QApplication.instance().quit)
+
+        # Containers for layout styling
         tab_container = QWidget(self)
         tools_container = QWidget(self)
-        tools_container.setStyleSheet("background-color: #1B1F23")
-        tab_container.setStyleSheet("background-color: #23292E")
 
-        self.stacked_widget = QStackedWidget()
-
+        # Page navigation tabs initilization and configuring
         tabs = QVBoxLayout(tab_container)
         tabs.setSpacing(0)
-        self.next_button.setFixedWidth(80)
-        self.prev_button.setFixedWidth(80)
+        tabs.setContentsMargins(0,0,0,0)
+        self.next_button.setFixedWidth(100)
+        self.prev_button.setFixedWidth(100)
+
+        # Add to page navigation tabs
         tabs.addWidget(self.next_button)
         tabs.addWidget(self.prev_button)
+        tabs.addStretch(1)
         
-
+        # Add to main content pages
         main = QHBoxLayout()
         main.addWidget(tab_container)
         main.addWidget(self.stacked_widget)
         main.setSpacing(10)
 
-        close = QPushButton('')
-        close.setIcon(QIcon('exit24.png'))
-        close.clicked.connect(QApplication.instance().quit)
-
+        # Toolbar initialization and configuring
         toolbar = QHBoxLayout(tools_container)
         toolbar.setAlignment(Qt.AlignmentFlag.AlignRight)
+        toolbar.setContentsMargins(0,0,0,0)
+
+        # Add to toolbar
         toolbar.addWidget(close)
 
+        # Everything's layout initialization and configuring
         screen = QVBoxLayout()
-        screen.addWidget(tools_container)
-        screen.addLayout(main)
         screen.setSpacing(0)
         screen.setContentsMargins(0,0,0,0)
 
+        # Add to entire page
+        screen.addWidget(tools_container)
+        screen.addLayout(main)
+        
+        # Define app layout
         self.setLayout(screen)
-        tab_container.setStyleSheet("QPushButton { background-color: #373F47; color: white; border: none; padding: 3px; height: 50%;}"
+
+        # StyleSheets
+        tab_container.setStyleSheet( "QWidget { background-color: #23292E;}"
+                            "QPushButton { background-color: #23292E; color: white; border: none; padding: 3px; height: 50%;}"
                             "QPushButton:hover { background-color: #47525C; }"
                             "QPushButton:pressed { background-color: #505C68;}"
                             "QPushButton:disabled { color: #373F47;}")
+        
+        tools_container.setStyleSheet("QWidget { background-color: #1B1F23;}"
+                            #"QPushButton { background-color: #1B1F23; border: none; padding: 3px; height: 32px; width: 32px;}"
+                            "QPushButton { background-image: url('exit24.png'); border: none; width: 24px; height: 24px;}"
+                            "QPushButton:hover { background-color: #47525C; }"
+                            "QPushButton:pressed { background-color: #505C68;}")
     
         self.setStyleSheet("QWidget {background-color: #373F47; color: white;}")
 
@@ -146,12 +112,20 @@ class Example(QWidget):
         if new_index >= 0:
             self.stacked_widget.setCurrentIndex(new_index)
 
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPosition()
+
+    def mouseMoveEvent(self, event):
+        delta = QPoint(event.globalPosition().toPoint() - self.oldPos.toPoint())
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPosition()
+
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    ex = Example()
+    tg = TaxGui()
     for i in range(5):
-        ex.insert_page(QLabel(f'This is page {i+1}'))
-    ex.resize(800,600)
-    ex.show()
+        tg.insert_page(QLabel(f'This is page {i+1}'))
+    tg.resize(800,600)
+    tg.show()
     app.exec()
